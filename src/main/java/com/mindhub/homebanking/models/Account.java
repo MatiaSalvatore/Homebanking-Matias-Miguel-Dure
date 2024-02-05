9946cb87a0b8b273d19ae48e756a6e109e3438c1
MatiaSalvatore <matiasmigueldure@gmail.com>
2024-02-05 08:01:08 -0300

    Account done

---
+++ src/main/java/com/mindhub/homebanking/models/Account.java
@@ -0,0 +1,55 @@
+package com.mindhub.homebanking.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,generator="native")
+    private long id;
+    private String number;
+    private LocalDate creationDate;
+    private Double balance;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="owner_id")
+    private Client owner;
+
+    public Account() {
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Client getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Client owner) {
+        this.owner = owner;
+    }
+}
